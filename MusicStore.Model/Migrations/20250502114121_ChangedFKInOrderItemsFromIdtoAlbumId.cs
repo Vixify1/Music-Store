dabda03f7f98fb6799d9e187f8e9c8819c75d3fe
Albin_Hoxha <albin.hoxha@fshnstudent.info>
2025-05-02 15:49:17 +0200

    In the checkout when  creating the order , in new Order {} has only 3 entitites without the TotalAmount , CreatedAt which are default value 0 in db , also the formula of SubTotal is wrong So I added the redirection after checkout to payment/create ,

--- MusicStore.Model/Migrations/20250502114121_ChangedFKInOrderItemsFromIdtoAlbumId.cs
+++ MusicStore.Model/Migrations/20250502114121_ChangedFKInOrderItemsFromIdtoAlbumId.cs
@@ -5,7 +5,7 @@
 namespace MusicStore.Model.Migrations
 {
     /// <inheritdoc />
-    public partial class RenameOrderItemAlbumIdColumn : Migration
+    public partial class ChangedFKInOrderItemsFromIdtoAlbumId : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
